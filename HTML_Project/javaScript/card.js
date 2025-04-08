ebc255f94695570fee6917a86fc56ac59996718d
kimyongjun0129 <kimyongjun0129@naver.com>
2025-04-08 16:30:28 +0900

    경로 JS로 이동

--- HTML_Project/javaScript/card.js
+++ HTML_Project/javaScript/card.js
@@ -1,20 +1,20 @@
 /*멤버카드 클릭시 해당 페이지로 이동*/
 
 $("#card_LJY").click(async function () {
-    location.href = 'IntroHtml/LJYWEB.HTML';
+    location.href = 'HTML_Project/IntroHtml/LJYWeb.HTML';
 })
 $("#card_hyeonha").click(async function () {
-    location.href = 'IntroHtml/hyeonhaWEB.html';
+    location.href = 'HTML_Project/IntroHtml/hyeonhaWEB.html';
 })
 $("#card_WSV").click(async function () {
-    location.href = 'IntroHtml/WSVWEB.html';
+    location.href = 'HTML_Project/IntroHtml/WSVWeb.html';
 })
 $("#card_CHE").click(async function () {
-    location.href = 'IntroHtml/CHEWEB.html';
+    location.href = 'HTML_Project/IntroHtml/CHEWeb.html';
 })
 $("#card_HJ").click(async function () {
-    location.href = 'IntroHtml/HJ.html';
+    location.href = 'HTML_Project/IntroHtml/HJ.html';
 })
 $("#card_KYG").click(async function () {
-    location.href = 'IntroHtml/KYGWeb.html';
+    location.href = 'HTML_Project/IntroHtml/KYGWeb.html';
 })
