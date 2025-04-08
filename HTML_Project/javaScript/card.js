7a09bd49c59add1d7491b3b92620fb1ef13c2009
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2025-04-08 18:05:58 +0900

    복원

---
+++ HTML_Project/javaScript/card.js
@@ -0,0 +1,20 @@
+/*멤버카드 클릭시 해당 페이지로 이동*/
+
+$("#card_LJY").click(async function () {
+    location.href = 'HTML_Project/IntroHtml/LJYWeb.HTML';
+})
+$("#card_hyeonha").click(async function () {
+    location.href = 'HTML_Project/IntroHtml/hyeonhaWEB.html';
+})
+$("#card_WSV").click(async function () {
+    location.href = 'HTML_Project/IntroHtml/WSVWeb.html';
+})
+$("#card_CHE").click(async function () {
+    location.href = 'HTML_Project/IntroHtml/CHEWeb.html';
+})
+$("#card_HJ").click(async function () {
+    location.href = 'HTML_Project/IntroHtml/HJ.html';
+})
+$("#card_KYG").click(async function () {
+    location.href = 'HTML_Project/IntroHtml/KYGWeb.html';
+})
