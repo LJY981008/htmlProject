a5f1f6000780314d6988ce7b1edee7fd35768550
yongjun <96245652+kimyongjun0129@users.noreply.github.com>
2025-04-10 13:13:31 +0900

    Update firebase.js

--- HTML_Project/javaScript/firebase.js
+++ HTML_Project/javaScript/firebase.js
@@ -4,6 +4,9 @@ import {
   getFirestore,
   doc,
   deleteDoc,
+  startAt,
+  limit,
+  getCountFromServer,
   collection,
   addDoc,
   getDocs,
@@ -25,6 +28,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+
 export const infoData = await getDocs(collection(db, "info"));
 export const commentsData = await getDocs(collection(db, "comments"));
 
@@ -37,3 +41,54 @@ export async function regist(id, pw, name) {
     };
     const addedDocRef = await addDoc(collection(db, "info"), newUser);
 }
+
+export async function commentSaveToDB (id, name, text, ymd, today) {
+  const newComment = {
+    'id': id,
+    'name': name,
+    'comment': text,
+    'date': ymd,
+    'dateObj': today 
+  };
+  await addDoc(collection(db, "comments"), newComment);
+}
+
+// 댓글 총 갯수 반환
+export async function getCommentDocNum() {
+      const colRef = collection(getFirestore(), "comments");
+      const a = await getCountFromServer(colRef)
+      return a.data().count;
+}
+
+// 댓글 전체 반환
+export async function getAllCommentDocs() {
+  const q = query(collection(db, "comments"), orderBy("dateObj", "desc"));
+  return await getDocs(q);
+}
+
+// 댓글 Doc Index 값에 따라 반환
+export async function getCommentDocs(pageIndex, commentCursors) {
+  const commentsRef = collection(db, "comments");
+  const baseQuery = query(commentsRef, orderBy("dateObj", "desc"));
+
+  let q;
+
+  if (pageIndex === 0) {
+    q = query(baseQuery, limit(5));
+  } else {
+    const startDoc = commentCursors[pageIndex];
+    if (!startDoc) {
+      console.warn("없는 페이지입니다.");
+      return null;
+    }
+    q = query(baseQuery, startAt(startDoc), limit(5));
+  }
+
+  return await getDocs(q);
+}
+
+// 특정 댓글 삭제
+export async function deleteComment(commentId) {
+  await deleteDoc(doc(db, "comments", commentId));
+}
+
