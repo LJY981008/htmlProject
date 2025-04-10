cfdfb06a549ea5cf50f545b05b237f76c7ed22fb
yongjun <96245652+kimyongjun0129@users.noreply.github.com>
2025-04-10 15:54:56 +0900

    Update firebase.js

--- HTML_Project/javaScript/firebase.js
+++ HTML_Project/javaScript/firebase.js
@@ -16,13 +16,13 @@ import {
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDIYr5xaD01ndxx0anOhCgoKRHT6rAHWnc",
-  authDomain: "weeklyme-4e92d.firebaseapp.com",
-  projectId: "weeklyme-4e92d",
-  storageBucket: "weeklyme-4e92d.firebasestorage.app",
-  messagingSenderId: "499565676750",
-  appId: "1:499565676750:web:60aa159f30a4917fe6689f",
-  measurementId: "G-2D8RFG5CVT"
+  apiKey: "AIzaSyCZmltuIqRXs2LWyGXKuaYHsFBkoCb4pjQ",
+  authDomain: "sparta-d3979.firebaseapp.com",
+  projectId: "sparta-d3979",
+  storageBucket: "sparta-d3979.firebasestorage.app",
+  messagingSenderId: "337696474068",
+  appId: "1:337696474068:web:44605bbd0732619596cc41",
+  measurementId: "G-PND6DC8KJR",
 };
 
 const app = initializeApp(firebaseConfig);
