5037bdd9906d1fd0d761b4c7a7e1957008e9a911
saevit <woosaevit@gmail.com>
2025-04-10 15:10:42 +0900

    주석

--- HTML_Project/javaScript/cookies.js
+++ HTML_Project/javaScript/cookies.js
@@ -1,13 +1,16 @@
+//쿠키 저장
 export function madeCookie(myID, myName) {
-    document.cookie = `myID=${myID}; max-age=180; path=/`; //max-age 초단위 시간 지속 (조정하면서 테스트 해 주세요)
+    document.cookie = `myID=${myID}; max-age=180; path=/`; //max-age 초단위 시간 지속 : 3분
     document.cookie = `myName=${myName}; max-age=180; path=/`;
 
-    //쿠키 확인
+    //콘솔 확인용
     setTimeout(() => {
         const cookieID = document.cookie.split('; ').find(row => row.startsWith('myID='))?.split('=')[1];
         console.log("저장된 myID:", cookieID);
     }, 200);
 }
+
+//쿠키 불러오기
 export function cheakedCookie() {
     const cookieName = document.cookie
         .split('; ')
@@ -20,6 +23,7 @@ export function cheakedCookie() {
         ?.split('=')[1];
 
     if (cookieName && cookieID) {
+        //콘솔 확인용
         console.log("불러온 myName:", cookieName, " 불러온 myID:", cookieID);
 
         const myInfo = {
@@ -37,6 +41,7 @@ export function cheakedCookie() {
         return myInfo;
     }
 }
+//쿠키 삭제
 export function removeCookie(){
     document.cookie = "myID=; max-age=0; path=/";
     document.cookie = "myName=; max-age=0; path=/";
